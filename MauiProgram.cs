bf433e4f25efec0ae2cc530978e789ab514d2e65
Echo3301 <angelicabergstroms@gmail.com>
2025-02-02 00:54:14 +0100

    01134

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -13,6 +13,7 @@ namespace MAUICalculator
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Gruppo-Regular.ttf", "GruppoRegular");
                 });
 
 #if DEBUG
